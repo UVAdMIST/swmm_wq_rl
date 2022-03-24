af801a44391f4451223381af27cc305a3d7eb16f
bdb3m <22157448+bdb3m@users.noreply.github.com>
2022-03-24 19:47:16 -0400

    final upload

--- rbc/run_hague_rbc_dtn.py
+++ rbc/run_hague_rbc_dtn.py
@@ -14,10 +14,10 @@ import subprocess
 import json
 import pandas as pd
 from swmm_hague_rbc import control_rules
-from swmm_hague_rbc import swmm_utils
+from swmm_rl_hague import swmm_utils
 from swmm_rl_hague.read_rpt import get_ele_df, get_file_contents, get_summary_df, get_total_flooding, get_mass_reacted
 import pyswmm
-pyswmm.lib.use("swmm5_hs_520dev6")
+pyswmm.lib.use("swmm5_hs_520dev6")  # use swmm with hot start functionality
 from pyswmm import Simulation, Nodes, Links
 
 control_time_step = 900  # control time step in seconds
@@ -37,8 +37,8 @@ start_time = datetime.datetime.now()
 
 with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
     sim.step_advance(control_time_step)
-    sim.start_time = datetime.datetime(2017, 1, 1, 0, 0, 0)  # change start time here
-    sim.end_time = datetime.datetime(2019, 11, 1, 0, 0, 0)  # change end time here
+    sim.start_time = datetime.datetime(2019, 8, 1, 0, 0, 0)  # change start time here
+    sim.end_time = datetime.datetime(2019, 9, 1, 0, 0, 0)  # change end time here
     previous_step = sim.start_time
     node_object = Nodes(sim)  # init node object
     St1 = node_object["st1"]
@@ -56,7 +56,7 @@ with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
 
     # calculate radius (ft) of storage units for GWL calculations
     St1_rad = math.sqrt(100000 / math.pi)
-    St2_rad = math.sqrt(20000 / math.pi)
+    # St2_rad = math.sqrt(20000 / math.pi)
     St3_rad = math.sqrt(50000 / math.pi)
 
     step_count = 1  # init counter for control step
@@ -68,11 +68,10 @@ with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
 
         # calculate GW exchange
         # current_gwl = gwl_df.iloc[gwl_df.index.get_loc(sim.current_time, method='nearest')]["IDW"]
-        # # print(sim.current_time, current_gwl)
-        # St1_gwq = swmm_utils.calc_gwl(St1.depth - abs(St1.invert_elevation), current_gwl, St1_rad)
-        # St2_gwq = swmm_utils.calc_gwl(St2.depth - abs(St2.invert_elevation), current_gwl, St2_rad)
-        # St3_gwq = swmm_utils.calc_gwl(St3.depth - abs(St3.invert_elevation), current_gwl, St3_rad)
-        # print(St1_gwq)
+        # print(sim.current_time, current_gwl)
+        # St1_gwq = swmm_utils.calc_dupuit(St1.depth, St1.invert_elevation, current_gwl, St1_rad)  #  - abs(St1.invert_elevation)
+        # St2_gwq = calc_gwl(St2.depth - abs(St2.invert_elevation), current_gwl, St2_rad)
+        # St3_gwq = swmm_utils.calc_dupuit(St3.depth, St3.invert_elevation, current_gwl, St3_rad)  #  - abs(St3.invert_elevation)
 
         # print("pregwl", St1.total_inflow)
         # add (or subtract) gw volume to storage unit
@@ -89,7 +88,7 @@ with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
         current_fcst = fcst_data.iloc[fcst_data.index.get_loc(current_dt, method='nearest')]  # ["IDW"]
         # print("forecast read")
 
-        if sum(current_fcst[:int(len(current_fcst)/2)]) > 1.:  # if storm in forecast check for flooding
+        if sum(current_fcst[:int(len(current_fcst)/2)]) > 0.25:  # if storm in forecast check for flooding (1inch for study 3)
             # if event_dict['total'] > 0.5:  # if rain in forecast check for flooding
             # if St1_drain_timer <= 0 or St2_drain_timer <= 0:  # check if either valve control timer expired
             # current_dt = sim.current_time
@@ -108,20 +107,8 @@ with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
             fcst_submodel = subprocess.check_output("C:/Anaconda2/envs/py36rbc/python.exe C:/PycharmProjects/swmm_hague_rbc/run_hague_fcst.py".split())
             # fcst_submodel = subprocess.run("C:/Anaconda2/envs/py36rbc/python.exe C:/PycharmProjects/swmm_hague_rbc/run_hague_fcst2.py")
             submodel_return = fcst_submodel.decode('utf-8')
-            # st1_fcst_vol = 0
-            # st3_fcst_vol = 0
-            # fcst_rpt = get_file_contents("C:/PycharmProjects/swmm_hague_rbc/cmac_temp/temp_inp.rpt")
-            # fcst_flood = get_total_flooding(fcst_rpt)
-            # if fcst_flood > 0:
-            #     fcst_df = get_summary_df(fcst_rpt, "Node Flooding Summary")
-            #     if "st1" in fcst_df.index:
-            #         st1_fcst_vol = fcst_df.loc["st1"]["Total_Vol"]
-            #     if "st3" in fcst_df.index:
-            #         st3_fcst_vol = fcst_df.loc["st3"]["Total_Vol"]
-            # flood_dict = json.loads(submodel_return)  # TODO fix reading json returned by subprocess
+
             flood_dict = json.loads(str('{') + submodel_return.split('{')[1].split('}')[0] + str('}'))
-            # print(current_step, current_dt, flood_dict)
-            # print("submodel done")
 
             # check report file to see if storage units flooded and calculate new valve positions if needed
             if flood_dict["St1"] > 0:
@@ -215,59 +202,6 @@ with Simulation(swmm_inp) as sim:  # loop through all steps in the simulation
         if St3.flooding > 0:
             R3.target_setting = 1.
 
-            # print("St1 timers: ", St1_drain_timer, St1_retention_timer, St1_drawdown_timer, R1.current_setting)
-            # print("St2 timers: ", St2_drain_timer, St2_retention_timer, St2_drawdown_timer, R2.current_setting)
-
-            # record system state for current time step
-            # total_flood.append((St1.statistics['flooding_volume'] + St2.statistics['flooding_volume'] +
-            #                     J1.statistics['flooding_volume']) * 7.481 / 1e6)  # cumulative vol in 10^6 gallons
-            # time.append(sim.current_time)
-            # St1_gwflow.append(St1_gwq), St2_gwflow.append(St2_gwq), St3_gwflow.append(St3_gwq),
-            # St1_depth.append(St1.depth), St2_depth.append(St2.depth), St3_depth.append(St3.depth)
-            # St1_flooding.append(St1.flooding), St2_flooding.append(St2.flooding), St3_flooding.append(St3.flooding)
-            # St1_fld_vol.append((St1.statistics['flooding_volume'] * 7.481 - sum(St1_fld_vol)))  # incremental vol in gal
-            # St2_fld_vol.append((St2.statistics['flooding_volume'] * 7.481 - sum(St2_fld_vol)))
-            # St3_fld_vol.append((St3.statistics['flooding_volume'] * 7.481 - sum(St3_fld_vol)))
-            # St1_full.append(St1.full_depth), St2_full.append(St2.full_depth), St3_full.append(St3.full_depth)
-            # St1_flow.append(St1.total_inflow * 28.3168)  # pond inflow, converted to L to calculate pollutant mass
-            # St2_flow.append(St2.total_inflow * 28.3168)
-            # St3_flow.append(St3.total_inflow * 28.3168)
-            # R1_flow.append(R1.flow), R2_flow.append(R2.flow), R3_flow.append(R3.flow)
-            # R1_act.append(R1.current_setting), R2_act.append(R2.current_setting), R3_act.append(R3.current_setting)
-            # St1_TSS_Load.append(St1.pollut_quality['TSS'] * St1.volume * 28.3168 / 453592)  # TSS mass in pond after treatment (lbs)
-            # St2_TSS_Load.append(St2.pollut_quality['TSS'] * St2.volume * 28.3168 / 453592)
-            # St3_TSS_Load.append(St3.pollut_quality['TSS'] * St3.volume * 28.3168 / 453592)
-            # St1_TP_Load.append(St1.pollut_quality['TP'] * St1.volume * 28.3168 / 453592)  # TP mass in pond after treatment (lbs)
-            # St2_TP_Load.append(St2.pollut_quality['TP'] * St2.volume * 28.3168 / 453592)
-            # St3_TP_Load.append(St3.pollut_quality['TP'] * St3.volume * 28.3168 / 453592)
-            # St1_TN_Load.append(St1.pollut_quality['TN'] * St1.volume * 28.3168 / 453592)  # TN mass in pond after treatment (lbs)
-            # St2_TN_Load.append(St2.pollut_quality['TN'] * St2.volume * 28.3168 / 453592)
-            # St3_TN_Load.append(St3.pollut_quality['TN'] * St3.volume * 28.3168 / 453592)
-            # St1_TSS_Conc.append(St1.pollut_quality['TSS'])  # TSS concentration after treatment (mg/L)
-            # St2_TSS_Conc.append(St2.pollut_quality['TSS'])
-            # St3_TSS_Conc.append(St3.pollut_quality['TSS'])
-            # St1_TP_Conc.append(St1.pollut_quality['TP'])  # TP concentration after treatment (mg/L)
-            # St2_TP_Conc.append(St2.pollut_quality['TP'])
-            # St3_TP_Conc.append(St3.pollut_quality['TP'])
-            # St1_TN_Conc.append(St1.pollut_quality['TN'])  # TN concentration after treatment (mg/L)
-            # St2_TN_Conc.append(St2.pollut_quality['TN'])
-            # St3_TN_Conc.append(St3.pollut_quality['TN'])
-            # R1_TSS_Load.append(R1.total_loading['TSS'])  # pounds of TSS passing through orifice
-            # R2_TSS_Load.append(R2.total_loading['TSS'])
-            # R3_TSS_Load.append(R3.total_loading['TSS'])
-            # R1_TSS_Conc.append(R1.pollut_quality['TSS'])  # TSS concentration passing through orifice
-            # R2_TSS_Conc.append(R2.pollut_quality['TSS'])
-            # R3_TSS_Conc.append(R3.pollut_quality['TSS'])
-            # R1_TP_Load.append(R1.total_loading['TP'])  # pounds of TP passing through orifice
-            # R2_TP_Load.append(R2.total_loading['TP'])
-            # R3_TP_Load.append(R3.total_loading['TP'])
-            # R1_TN_Load.append(R1.total_loading['TN'])  # pounds of TN passing through orifice
-            # R2_TN_Load.append(R2.total_loading['TN'])
-            # R3_TN_Load.append(R3.total_loading['TN'])
-            # R1_TSS_inc.append(R1.total_loading['TSS'] - R1_TSS[-1])  # cumulative pounds of TSS passing through orifice
-            # R2_TSS_inc.append(R2.total_loading['TSS'] - R2_TSS[-1])
-            # R3_TSS_inc.append(R3.total_loading['TSS'] - R3_TSS[-1])
-
             current_step += 1
         step_count += 1
     sim.report()
@@ -330,45 +264,7 @@ rpt_df['St3_Max'] = 6.56
 rpt_df['Total_Flood'] = total_flood
 rpt_df['Mass_Reacted'] = tss_reacted
 
-rpt_df.to_csv(os.path.join(project_dir, "cmac_results/all_data/2017_2019_dntrbc_v22_mass.csv"), index=True)
+rpt_df.to_csv(os.path.join(project_dir, "cmac_results/082019_dntrbc_v22_mass_025thrshld.csv"), index=True)
 
 end_time = datetime.datetime.now()
 print("\n run time (hr): ", end_time - start_time)
-
-# # put result lists in dictionary
-# # read observed rain and tide data
-# obs_rain_path = "C:/Users/Ben Bowes/Documents/LongTerm_SWMM/Clean_rain_data/rain_cleaned_combined.csv"
-# obs_tide_path = "C:/Users/Ben Bowes/Documents/LongTerm_SWMM/Raw_tide_data/tide_15min_2010_2019.csv"
-# obs_rain_df = pd.read_csv(obs_rain_path, index_col="datetime", infer_datetime_format=True, parse_dates=True)
-# obs_tide_df = pd.read_csv(obs_tide_path, index_col="datetime", infer_datetime_format=True, parse_dates=True)
-# # slice observed data to match simulation results
-# # start = datetime.datetime(2019, 8, 1, 0, 0, 0)  # change start time here
-# # end = datetime.datetime(2019, 9, 1, 0, 0, 0)  # change end time here
-# start = time[0]  # change start time here
-# end = time[-1]  # change end time here
-#
-# obs_rain_df = obs_rain_df.loc[start: end]
-# obs_tide_df = obs_tide_df.loc[start: end]
-# env_df = pd.concat([obs_rain_df, obs_tide_df], axis=1)
-# env_df.reset_index(inplace=True)
-
-# result_list = [time, St1_depth, St2_depth, St3_depth, St1_flooding, St2_flooding, St3_flooding,
-#                St1_fld_vol, St2_fld_vol, St3_fld_vol, St1_TSS_Load, St2_TSS_Load, St3_TSS_Load,
-#                R1_TSS_Load, R2_TSS_Load, R3_TSS_Load, R1_TSS_Conc, R2_TSS_Conc, R3_TSS_Conc,
-#                St1_gwflow, St2_gwflow, St3_gwflow, St1_flow, St2_flow, St3_flow,
-#                St1_full, St2_full, St3_full, R1_act, R2_act, R3_act]
-# result_cols = ["Datetime", "St1_depth", "St2_depth", "St3_depth", "St1_flooding", "St2_flooding", "St3_flooding",
-#                "St1_fld_vol", "St2_fld_vol", "St3_fld_vol", "St1_TSS_Load", "St2_TSS_Load", "St3_TSS_Load",
-#                "R1_TSS_Load", "R2_TSS_Load", "R3_TSS_Load", "R1_TSS_Conc", "R2_TSS_Conc", "R3_TSS_Conc",
-#                "St1_gwflow", "St2_gwflow", "St3_gwflow", "St1_flow", "St2_flow", "St3_flow",
-#                "St1_full", "St2_full", "St3_full", "R1_act", "R2_act", "R3_act"]
-# results_df = pd.DataFrame(result_list).transpose()
-# results_df.columns = result_cols
-# results_df = pd.concat([results_df, df], axis=1)
-
-# result_dict = {}
-# for key, value in zip(result_cols, result_list):
-#     result_dict[key] = value
-#
-# # plot results TODO plot RBC results
-# swmm_utils.plot_ctl_results(env_df, result_dict, "082019_v22_thrshld05", os.path.join(project_dir, "cmac_results"))
